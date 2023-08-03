cefe84ccf8e1b701296aafedda7a1423428fbc28
liushenghao <1243369404@qq.com>
2023-08-03 11:46:01 +0800

    (Finish createAppid module and add uploadLogs module)

---
+++ src/main/java/com/cvte/logsystem/db_mongo/repository/BasicMongoRepository.java
@@ -0,0 +1,10 @@
+package com.cvte.logsystem.db_mongo.repository;
+
+/**
+ * @Description TODO
+ * @Classname BasicMongoRepository
+ * @Date 2023/8/2 5:33 PM
+ * @Created by liushenghao
+ */
+public interface BasicMongoRepository {
+}
---
+++ src/main/java/com/cvte/logsystem/db_mongo/repositoryImpl/MongoRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.cvte.logsystem.db_mongo.repositoryImpl;
+
+import com.cvte.logsystem.db_mongo.repository.BasicMongoRepository;
+
+/**
+ * @Description TODO
+ * @Classname MongoRepositoryImpl
+ * @Date 2023/8/2 5:34 PM
+ * @Created by liushenghao
+ */
+public class MongoRepositoryImpl implements BasicMongoRepository {
+}
